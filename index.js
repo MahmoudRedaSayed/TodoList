b5f26a03b21dee130c2f262ffd491129bcbb936e
MahmoudRedaSayed <anamahmoud10203040@gmail.com>
2022-07-12 19:32:12 +0200

    the database is connected to monogo atlas

--- index.js
+++ index.js
@@ -1,45 +1,54 @@
 const express = require("express");
 const body= require("body-parser");
+const mongoose = require('mongoose');
+mongoose.connect("mongodb+srv://MahmoudReda:Zi5yl2lRXTySMJyH@firstcluster.23iywgv.mongodb.net/test?retryWrites=true&w=majority",{ useNewUrlParser: true, useUnifiedTopology: true})
+.then(()=>console.log("the database is connected"))
+.catch(err=>console.log(err));
 const date=require(__dirname+"/Data.js");
 var app= express();
 app.use(body.urlencoded({extended:true}));
 app.use(express.static("public"));
 app.set("view engine","ejs");
-// the data of the list
-const items=["food ", "study" , "workout", "Gym"];
+const lists=mongoose.Schema({
+    ListName:String,
+    item:String
+});
 
-// the data of the list
-const work=[];
+const Items=mongoose.model("Items",lists);
 
 app.listen(3000,function(){
     console.log("the server is running now");
 })
-app.get("/",function (req,res){
-    console.log(date.dateDay);
-    res.render("list.ejs",{newListItems:items,listTitle:"Home"});
+app.get("/:listName",function (req,res){
+    const listName=req.params.listName;
+    Items.find({ListName:listName},function(err,items){
+    res.render("list.ejs",{newListItems:items,listTitle:listName});
+    });
+    
 })
 
 app.post("/",function (req,res){
-    var item=req.body.newItem;
+    var itemtext=req.body.newItem;
     var listName=req.body.list;
+    const item=new Items({ListName:listName,item:itemtext});
+    item.save();
+    res.redirect("/"+listName);
     
-    if(listName==="Home" )
-    {
-        if( item!=="")
-        items.push(item);
-        res.redirect("/");
-    }
-    else
-    {
-        if( item!=="")
-        work.push(item);
-        res.redirect("/work");
-    }
+})
+
+app.post("/:listName",function(req,res){
+    console.log("done");
+    var id=req.body.checkbox;
+    var listName=req.params.listName;
+    Items.findByIdAndRemove({_id:id},function(err){
+        if(!err)
+        {
+            console.log("the element is deleted ");
+        }
+    });
+    res.redirect("/"+listName);
 })
 
 
-app.get("/work",function (req,res){
-    res.render("list.ejs",{newListItems:work,listTitle:"Work"});
-})
 
 
